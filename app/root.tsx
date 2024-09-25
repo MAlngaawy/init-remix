48487802576b35abfdac73181f6412be57402468
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-09-25 18:12:40 +0300

    styles & Nested Routes and Outlets & Client Side Routing & Loading Data & URL Params in Loaders & Validating Params and Throwing Responses

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,36 @@
+// import types
+import type { LinksFunction } from "@remix-run/node";
+
+import { json } from "@remix-run/node";
+
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
 
+// existing imports
+import { getContacts } from "./data";
+
+import appStylesHref from "./app.css?url";
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -34,17 +58,37 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+             <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
           </nav>
         </div>
-
+        <div className="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
