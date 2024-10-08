711af03f2607b79791b3a5c8186c5c7199bdfe15
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-10-08 16:10:22 +0300

    create db controll

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,22 @@
+// import { PrismaClient } from '@prisma/client';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { db } from '~/db.server';
+
+// const prisma = new PrismaClient();
+
+export const loader = async () => {
+  const posts = await db.post.findMany();
+  const users = await db.user.findMany();
+
+  return json({ posts, users });
+};
+
 export default function Index() {
+  const { users, posts } = useLoaderData<typeof loader>();
+  console.log('USERS', users);
+  console.log('POSTS', posts);
+
   return (
     <p id="index-page">
       This is a demo for Remix.
